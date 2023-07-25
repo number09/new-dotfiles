1443bc365003242cac0b3cd319f5f1e9133c4f91
Koichi Ogura <12413803+number09@users.noreply.github.com>
2023-07-25 02:46:07 +0000

    fix install.sh

--- install.sh
+++ install.sh
@@ -21,18 +21,18 @@ has() {
 }
 
 if [ ! -d ${DOT_DIR} ]; then
-    if has "git"; then
-        git clone git@github.com:number09/new-dotfiles.git ${DOT_DIR}
-    else
-        echo "git required"
-        exit 1
-    fi
+    # if has "git"; then
+    #     git clone git@github.com:number09/new-dotfiles.git ${DOT_DIR}
+    # else
+    #    echo "git required"
+    #    exit 1
+    # fi
 
-    # TARBALL="https://github.com/number09/new-dotfiles/archive/main.tar.gz"
-    #   curl -L ${TARBALL} -o main.tar.gz
-    # tar -zxvf main.tar.gz
-    # rm -f main.tar.gz
-    # mv -f new-dotfiles-main "${DOT_DIR}"
+    TARBALL="https://github.com/number09/new-dotfiles/archive/main.tar.gz"
+    curl -L ${TARBALL} -o main.tar.gz
+    tar -zxvf main.tar.gz
+    rm -f main.tar.gz
+    mv -f new-dotfiles-main "${DOT_DIR}"
 
     cd ${DOT_DIR} && make init && make mac_setup && make install-common
 else
